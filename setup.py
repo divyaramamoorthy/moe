99491b03595fe0249edaf0821359e51f457caee2
Divya Ramamoorthy <25695820+divyaramamoorthy@users.noreply.github.com>
2025-02-13 00:15:48 -0500

    clean repo structure

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="moe",  # Replace with your project name
+    name="moe", 
     version="0.1",
     packages=find_packages(),
     install_requires=[
